fc4ed9d7cd5e5861196847f2ef4d15de871c3c45
Sammyr-7S <samitolojkyt@gmail.com>
2025-11-06 11:01:56 -0500

    Agrego mi parte del código para Taller-Cohorte-3 (Maria Jose)

--- BankFutco/src/services/LoansService.java
+++ BankFutco/src/services/LoansService.java
@@ -1,6 +1,38 @@
 package services;
 
+import java.util.List;
+import java.util.Optional;
+import model.Loans;
+import repositories.LoansRepository;
 
-public class LoansService {
+public class LoansService implements ILoansService {
+    private final LoansRepository repository;
 
+    public LoansService() {
+        this(new LoansRepository());
+    }
+
+    public LoansService(LoansRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Loans save(Loans loan) {
+        return repository.save(loan);
+    }
+
+    @Override
+    public Optional<Loans> findById(String id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Loans> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return repository.deleteById(id);
+    }
 }
